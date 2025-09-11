e76f587f3d910610ab76d6cd0873c0b5810a53d6
Orliluq <orlibet@gmail.com>
2025-09-10 21:23:20 -0400

    profile session

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,21 +1,28 @@
-import { useSession } from "next-auth/react";
-import Image from "next/image";
+// "use client";
 
-export default function Profile() {
-  const { data: session } = useSession();
+// // import { useSession } from "next-auth/react";
+// import Image from "next/image";
+// import { getServerSession } from "next-auth";
 
-  return (
-    <div>
-      <h1>Hola {session?.user?.name}</h1>
-      {session?.user?.image && (
-        <Image
-          src={session.user.image}
-          alt="Avatar"
-          width={80}
-          height={80}
-          className="rounded-full"
-        />
-      )}
-    </div>
-  );
-}
+// export default async function Profile() {
+//   const session = await getServerSession();
+
+//   if (!session) {
+//     return <p>No autenticado</p>;
+//   }
+
+//   return (
+//     <div>
+//       <h1>Hola {session?.user?.name}</h1>
+//       {session?.user?.image && (
+//         <Image
+//           src={session.user.image}
+//           alt="Avatar"
+//           width={80}
+//           height={80}
+//           className="rounded-full"
+//         />
+//       )}
+//     </div>
+//   );
+// }
